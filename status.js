7dfb21d9b9ec592e692545f44f2aa260329f146f
deanOfWalls <deanofwalls@gmail.com>
2023-08-16 03:35:04 -0400

    Modified timer for loading screen transition

--- status.js
+++ status.js
@@ -12,7 +12,7 @@ function checkAppStatus() {
                 setTimeout(() => {
                     document.getElementById('loadingSection').style.display = 'none';
                     document.getElementById('mainAppSection').style.display = 'block';
-                }, 1000);
+                }, 2500);
             } else {
                 setTimeout(checkAppStatus, 5000);
             }
