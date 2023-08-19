91b468c3aa69a6246cbe2a43e45bf04b96948601
deanOfWalls <deanofwalls@gmail.com>
2023-08-19 19:09:09 -0400

    Troubleshooting loading sequence

--- status.js
+++ status.js
@@ -1,20 +1,34 @@
 document.addEventListener("DOMContentLoaded", function() {
-    setTimeout(function() {
-        checkAppStatus();
-    }, 5000);
+    setTimeout(checkAppStatus, 5000);
 });
 
 function checkAppStatus() {
     fetch('https://crud-cors-back.onrender.com/health-check')
-        .then(response => response.json())
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
         .then(data => {
-            document.getElementById('loadingSection').style.display = 'none';
-            document.getElementById('mainAppSection').style.display = 'block';
-            document.getElementById('statsSection').style.display = 'block';
+            if (data) {
+                document.getElementById('loadingSection').style.display = 'none';
+                document.getElementById('mainAppSection').style.display = 'block';
+                document.getElementById('statsSection').style.display = 'block';
 
-            document.querySelector("#cpuCount .stat-value").textContent = data.cpu;
-            document.querySelector("#cpuSpeed .stat-value").textContent = data.memory;
-            document.querySelector("#jvmMemory .stat-value").textContent = data.uptime;
+                // Ensure that data has the properties you are looking for before setting them
+                if (data.cpu) {
+                    document.querySelector("#cpuCount .stat-value").textContent = data.cpu;
+                }
+                if (data.memory) {
+                    document.querySelector("#cpuSpeed .stat-value").textContent = data.memory;
+                }
+                if (data.uptime) {
+                    document.querySelector("#jvmMemory .stat-value").textContent = data.uptime;
+                }
+            } else {
+                console.error('Received data is undefined or not in the expected format.');
+            }
         })
         .catch(error => {
             console.error('There was a problem with the fetch operation:', error.message);
