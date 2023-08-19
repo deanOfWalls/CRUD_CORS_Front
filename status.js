3d988c0f44f221e4935aa4f227934fc4e853d6a9
deanOfWalls <deanofwalls@gmail.com>
2023-08-19 19:20:12 -0400

    Troubleshooting loading sequence

--- status.js
+++ status.js
@@ -1,34 +1,31 @@
 document.addEventListener("DOMContentLoaded", function() {
-    setTimeout(checkAppStatus, 5000);
+    setTimeout(function() {
+        checkAppStatus();
+    }, 5000);
 });
 
 function checkAppStatus() {
     fetch('https://crud-cors-back.onrender.com/health-check')
         .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
+            // Check if the content type is JSON
+            if (response.headers.get('Content-Type').includes('application/json')) {
+                return response.json();
+            } else {
+                // Log the raw response for further inspection
+                return response.text().then(text => {
+                    console.log("Raw Response:", text);
+                    throw new Error('Received non-JSON response');
+                });
             }
-            return response.json();
         })
         .then(data => {
-            if (data) {
-                document.getElementById('loadingSection').style.display = 'none';
-                document.getElementById('mainAppSection').style.display = 'block';
-                document.getElementById('statsSection').style.display = 'block';
+            document.getElementById('loadingSection').style.display = 'none';
+            document.getElementById('mainAppSection').style.display = 'block';
+            document.getElementById('statsSection').style.display = 'block';
 
-                // Ensure that data has the properties you are looking for before setting them
-                if (data.cpu) {
-                    document.querySelector("#cpuCount .stat-value").textContent = data.cpu;
-                }
-                if (data.memory) {
-                    document.querySelector("#cpuSpeed .stat-value").textContent = data.memory;
-                }
-                if (data.uptime) {
-                    document.querySelector("#jvmMemory .stat-value").textContent = data.uptime;
-                }
-            } else {
-                console.error('Received data is undefined or not in the expected format.');
-            }
+            document.querySelector("#cpuCount .stat-value").textContent = data.cpu;
+            document.querySelector("#cpuSpeed .stat-value").textContent = data.memory;
+            document.querySelector("#jvmMemory .stat-value").textContent = data.uptime;
         })
         .catch(error => {
             console.error('There was a problem with the fetch operation:', error.message);
