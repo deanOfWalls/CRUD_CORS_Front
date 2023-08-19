4361e34a3b9977a652d0e812102016bb62e897d4
deanOfWalls <deanofwalls@gmail.com>
2023-08-19 18:56:39 -0400

    Troubleshooting loading sequence

--- status.js
+++ status.js
@@ -1,30 +1,22 @@
-document.addEventListener("DOMContentLoaded", () => {
-    document.getElementById('loadingSection').style.display = 'block';
-    document.querySelector('.form-container').style.display = 'none';
-    document.getElementById('statsSection').style.display = 'none';
-
-    setTimeout(() => {
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(function() {
         checkAppStatus();
     }, 5000);
 });
 
 function checkAppStatus() {
     fetch('https://crud-cors-back.onrender.com/health-check')
-        .then(response => {
-            if (response.status === 200) return response.text();
-            throw new Error('Not up yet');
-        })
-        .then(status => {
+        .then(response => response.json())
+        .then(data => {
             document.getElementById('loadingSection').style.display = 'none';
+            document.getElementById('mainAppSection').style.display = 'block';
             document.getElementById('statsSection').style.display = 'block';
-            document.querySelector('.form-container').style.display = 'block';
 
-            const stats = JSON.parse(status);
-            document.querySelector("#cpuCount .stat-value").textContent = stats.cpu;
-            document.querySelector("#cpuSpeed .stat-value").textContent = stats.memory;
-            document.querySelector("#jvmMemory .stat-value").textContent = stats.uptime;
-
-        }).catch(error => {
-            console.log(error);
+            document.querySelector("#cpuCount .stat-value").textContent = data.cpu;
+            document.querySelector("#cpuSpeed .stat-value").textContent = data.memory;
+            document.querySelector("#jvmMemory .stat-value").textContent = data.uptime;
+        })
+        .catch(error => {
+            console.error('There was a problem with the fetch operation:', error.message);
         });
 }
