a20dc12df09dea59061ac286ec418be5f4afe87a
deanOfWalls <deanofwalls@gmail.com>
2023-08-19 19:35:02 -0400

    Troubleshooting loading sequence

--- status.js
+++ status.js
@@ -1,36 +1,25 @@
-document.addEventListener("DOMContentLoaded", function() {
-    setTimeout(checkAppStatus, 5000);
-});
-
 function checkAppStatus() {
     fetch('https://crud-cors-back.onrender.com/health-check')
-        .then(response => response.text())  // fetch the response as text first
-        .then(text => {
-            try {
-                return JSON.parse(text); // Try parsing the text to JSON
-            } catch (error) {
-                console.error('Failed to parse JSON:', text);
-                throw new Error('Received non-JSON response');
+        .then(response => {
+            if (response.status === 200) {
+                return response.text();
             }
+            throw new Error('Not up yet');
         })
-        .then(data => {
-            if(data && data.cpu && data.memory && data.uptime) {
-                document.getElementById('loadingSection').style.display = 'none';
-                document.getElementById('mainAppSection').style.display = 'block';
-                document.getElementById('statsSection').style.display = 'block';
-
-                document.querySelector("#cpuCount .stat-value").textContent = data.cpu;
-                document.querySelector("#cpuSpeed .stat-value").textContent = data.memory;
-                document.querySelector("#jvmMemory .stat-value").textContent = data.uptime;
+        .then(status => {
+            if (status === 'UP') {
+                // Introduce a 5-second delay before switching views
+                setTimeout(() => {
+                    document.getElementById('loadingSection').style.display = 'none';
+                    document.getElementById('mainAppSection').style.display = 'block';
+                }, 4500);
             } else {
-                throw new Error('Incomplete or missing data fields in response');
+                setTimeout(checkAppStatus, 5000);
             }
         })
         .catch(error => {
-            console.error('There was a problem with the fetch operation:', error.message);
-            // If there's an error, we should at least hide the loading screen
-            document.getElementById('loadingSection').style.display = 'none';
-            document.getElementById('mainAppSection').style.display = 'block';
-            document.getElementById('statsSection').style.display = 'none'; // Hide stats if there's an error
+            setTimeout(checkAppStatus, 5000);
         });
 }
+
+checkAppStatus();
